a6cc9dc744fbcba64215db85ea1e4eff44ee3f1c
akochepasov <akochepasov@aerovel.com>
2025-10-16 18:06:25 -0700

    More sorting tests

--- sorting/sorting.cpp
+++ sorting/sorting.cpp
@@ -1,7 +1,25 @@
 #include <drillheader.h>
-
 #include <gtest/gtest.h>
+#include <functional>
 
+using ArrayGenerator = std::function<void(std::vector<int>&)>;
+
+// Array generators
+namespace gen {
+    void uniform_random(std::vector<int>& arr) {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<int> dist(-100, 100);
+        std::generate(ALL(arr), [&](){ return dist(gen); });
+    }
+
+    void sequential(std::vector<int>& arr) {
+        std::iota(arr.begin(), arr.end(), 0);
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::shuffle(arr.begin(), arr.end(), gen);
+    }
+}
 
 void InsertionSort(std::vector<int>& arr) {
     FOR(i, 1, SZ(arr)) {
@@ -29,37 +47,55 @@ void BubbleSort(std::vector<int>& arr) {
     }
 }
 
-class SortingTest : public testing::Test {
-    // If unsorted, sort them first
-    protected:
-        std::random_device rd;
-        constexpr static size_t N = 100;
-        std::vector<int> arr;
-        std::vector<int> expected;
-    public:
-        void SetUp() {
-            std::mt19937 gen(rd());
-            std::uniform_int_distribution<int> dist(-100, 100);
+struct ArrayParams {
+    ArrayGenerator generator;
+    size_t size;
+    std::string name;
 
-            arr.resize(N);
-
-            // std::iota(ALL(arr), 0);
-            // std::shuffle(ALL(arr), gen);
-            std::generate_n(arr.begin(), N, [&](){ return dist(gen); });
-
-            expected.assign(ALL(arr));
-            std::sort(ALL(expected));
-        };
+    ArrayParams(ArrayGenerator gen, size_t sz, std::string n) 
+        : generator(gen), size(sz), name(std::move(n)) {}
 };
 
-TEST_F(SortingTest, InsertionSort) {
+class SortingTest : public testing::TestWithParam<ArrayParams> {
+protected:
+    std::vector<int> arr;
+    std::vector<int> expected;
+
+public:
+    void SetUp() override {
+        const auto& params = GetParam();
+        arr.resize(params.size);
+        params.generator(arr);
+        
+        expected.assign(ALL(arr));
+        std::sort(ALL(expected));
+    }
+};
+
+TEST_P(SortingTest, InsertionSort) {
     InsertionSort(arr);
-
     EXPECT_EQ(arr, expected);
 }
 
-TEST_F(SortingTest, BubbleSort) {
+TEST_P(SortingTest, BubbleSort) {
     BubbleSort(arr);
-
     EXPECT_EQ(arr, expected);
 }
+
+std::vector<ArrayParams> GenerateTestCases() {
+    return {
+        ArrayParams(gen::uniform_random, 100, "UniformRandom100"),
+        ArrayParams(gen::uniform_random, 1'000, "UniformRandom1M"),
+        ArrayParams(gen::sequential, 100, "Sequential100"),
+        ArrayParams(gen::sequential, 1'000, "Sequential1M"),
+    };
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    SortingTestSuite,
+    SortingTest,
+    testing::ValuesIn(GenerateTestCases()),
+    [](const testing::TestParamInfo<ArrayParams>& info) {
+        return info.param.name;
+    }
+);
