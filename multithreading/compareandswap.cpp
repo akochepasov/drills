17d64d2a16b8fb2214351c1da539c086650604ce
Anton <akochepasov@users.noreply.github.com>
2026-01-02 00:29:28 -0800

    CAS tweaks

--- multithreading/compareandswap.cpp
+++ multithreading/compareandswap.cpp
@@ -1,3 +1,7 @@
+//#define NOMINMAX
+//#define WIN32_LEAN_AND_MEAN
+//#include <windows.h> // For InterlockedCompareExchange
+
 #include <mutex>
 #include <thread>
 #include <chrono>
@@ -25,16 +29,15 @@ TEST(CAS, raw) {
 	std::mutex start_mtx;
 	std::condition_variable start_cv;
 	bool start_flag = false;
-    std::atomic<int> ready_count = 0;
+    std::atomic<int> ready_count{ threads_count };
 
 	std::vector<std::thread> threads;
     for (int t = 0; t < threads_count; t++) {
         threads.emplace_back([t, N, threads_count, &sum, &start_mtx, &start_flag, &start_cv, &ready_count]() {
-            ready_count++;
+            ready_count--;
             {
                 std::unique_lock<std::mutex> lock(start_mtx);
                 start_cv.wait(lock, [&]() { return start_flag; });
-
 //                std::cout << "Thread " << t << " started, Range: [" << t * N / threads_count << ", " << (t + 1) * N / threads_count << ")\n";
             }
 
@@ -44,11 +47,13 @@ TEST(CAS, raw) {
                     expected = sum;
                     desired = expected + i;
                 } while (!compare_and_swap_raw((int64_t*)&sum, expected, desired));
+                // } while (InterlockedCompareExchange((uint64_t*)&sum, desired, expected) != expected); - 2x faster
+                // } while (!std::atomic_compare_exchange_strong((std::atomic<int64_t>*) & sum, &expected, desired));  - 10% faster
             }
         });
 	}
 
-    while (ready_count.load() < threads_count) {
+    while (ready_count.load()) {
         std::this_thread::yield();
     }
 
@@ -72,58 +77,3 @@ TEST(CAS, raw) {
 	EXPECT_EQ(sum, N * (N - 1) / 2);
 }
 
-// Same, with STL std::atomic_compare_exchange_strong
-TEST(CAS, stl) {
-    uint64_t N = 1'000'000;
-    uint64_t sum = 0;
-    int threads_count = 10;
-
-    std::mutex start_mtx;
-    std::condition_variable start_cv;
-    bool start_flag = false;
-    std::atomic<int> ready_count = 0;
-
-    std::vector<std::thread> threads;
-    for (int t = 0; t < threads_count; t++) {
-        threads.emplace_back([t, N, threads_count, &sum, &start_mtx, &start_flag, &start_cv, &ready_count]() {
-            ready_count++;
-            {
-                std::unique_lock<std::mutex> lock(start_mtx);
-                start_cv.wait(lock, [&]() { return start_flag; });
-
-//                std::cout << "Thread " << t << " started, Range: [" << t * N / threads_count << ", " << (t + 1) * N / threads_count << ")\n";
-            }
-
-            for (int i = t * N / threads_count; i < (t + 1) * N / threads_count; i++) {
-                int64_t expected, desired;
-                do {
-                    expected = sum;
-                    desired = expected + i;
-                } while (!std::atomic_compare_exchange_strong((std::atomic<int64_t>*)&sum, &expected, desired));
-            }
-            });
-    }
-
-    while (ready_count.load() < threads_count) {
-        std::this_thread::yield();
-    }
-
-    {
-        std::unique_lock<std::mutex> lock(start_mtx);
-        start_flag = true;
-        start_cv.notify_all();
-//        std::cout << "All threads started\n";
-    }
-
-    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
-
-    for (auto& thread : threads) {
-        thread.join();
-    }
-
-    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration = end - start;
-    std::cout << "Duration: " << duration.count() << " seconds" << std::endl;
-
-    EXPECT_EQ(sum, N * (N - 1) / 2);
-}
