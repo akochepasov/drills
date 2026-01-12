58912f1ba94ef42c9d80cba3d6e35f9c13aae3fe
Anton <akochepasov@users.noreply.github.com>
2026-01-11 16:39:35 -0800

    Check collisions and reformatting

--- two-pointers/two_pointers.cpp
+++ two-pointers/two_pointers.cpp
@@ -11,9 +11,9 @@ class SortedIntersectionTest : public testing::Test {
         std::vector<int> expected = {-20, 40};
 };
 
-TEST_F(SortedIntersectionTest, STL) {  
+TEST_F(SortedIntersectionTest, STL) {
     std::vector<int> result;
-    std::unordered_set<int> a_set(a.begin(), a.end());  
+    std::unordered_set<int> a_set(a.begin(), a.end());
     std::unordered_set<int> b_set(b.begin(), b.end());
 
     for (auto e : a) {
@@ -22,11 +22,11 @@ TEST_F(SortedIntersectionTest, STL) {
         }
     }
 
-    EXPECT_EQ(result, expected);  
+    EXPECT_EQ(result, expected);
 }
-  
+
 TEST_F(SortedIntersectionTest, Raw) {
-    std::vector<int> result;  
+    std::vector<int> result;
 
     size_t i = 0, j = 0;
     while (i < SZ(a) && j < SZ(b)) {
@@ -43,35 +43,38 @@ TEST_F(SortedIntersectionTest, Raw) {
     EXPECT_EQ(result, expected);
 }
 
-
 // Simple String Split
 // Question: Implement a function that splits a string by a delimiter character
-// and returns a vector of strings. 
+// and returns a vector of strings.
 
 VS SplitSimpleWithDelimiter(const std::string& str, char delimiter) {
-    std::vector<std::string> resulting_tokens; resulting_tokens.reserve(256);
+    std::vector<std::string> result; result.reserve(256);
 
-    if (str.size() == 0) {
-        resulting_tokens.push_back(str);
-        return resulting_tokens;
+    size_t strsz = SZ(str);
+    if (strsz == 0) {
+        result.push_back(str);
+        return result;
     }
 
-    size_t strsz = str.size();
-    size_t b = 0, e = 0;
-    while (b < strsz) {   // go across the whole strings
-        while (e < strsz && str[e] != delimiter)   // go till the delimilter
-            e++;
+    size_t e = 0;
+    FOR(b, 0, strsz) {   // go across the whole strings till the delimilter
+        while (e < strsz && str[e] != delimiter) e++;
 
-        resulting_tokens.emplace_back(str.substr(b, e - b));
-        b = e + 1;
-        e++;
+        result.emplace_back(str.substr(b, e - b));
+
+        b = e; e++;
     }
-    
-    return resulting_tokens;
+
+    return result;
 }
 
 VS SplitWithLibs(const std::string& str, char delimiter) {
-	VS result; result.reserve(256);
+    VS result; result.reserve(256);
+    if (SZ(str) == 0) {
+        result.push_back(str);
+        return result;
+    }
+
 	std::istringstream ss(str);
     std::string token;
     while (getline(ss, token, delimiter)) {
@@ -81,37 +84,74 @@ VS SplitWithLibs(const std::string& str, char delimiter) {
     return result;
 }
 
-class TokenizeText : public ::testing::Test {
-protected:
-    vector<VS> param_tokens =
-    {
+
+class VectorStringParams: public ::testing::TestWithParam<std::vector<std::string>> {};
+
+TEST_P(VectorStringParams, SplitSimpleWithDelimiter) {
+    char delimiter = ',';
+    auto str_param = GetParam();
+    const std::string& str = str_param[0];
+    VS exp_tokens(str_param.begin() + 1, str_param.end());
+
+    std::vector<std::string> act_tokens1 = SplitSimpleWithDelimiter(str, delimiter);
+    std::vector<std::string> act_tokens2 = SplitWithLibs(str, delimiter);
+
+    EXPECT_EQ(act_tokens1, exp_tokens);
+    EXPECT_EQ(act_tokens2, exp_tokens);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    TwoPointers,        // Test suite name prefix
+    VectorStringParams, // Fixture class
+    ::testing::Values(  // Values
         VS{"one,two,three", "one", "two", "three"},
         VS{ "hello", "hello" },
         VS{ "", "" }
-    };
-};
+    )
+);
 
-TEST_F(TokenizeText, SplitSimpleWithDelimiter) {
-    char delimiter = ',';
 
-    for (auto& str_param : param_tokens) {
-        const std::string& str = str_param[0];
-        VS expected_tokens(str_param.begin() + 1, str_param.end());
-        std::vector<std::string> resulting_tokens = SplitSimpleWithDelimiter(str, delimiter);
+int CheckCollision(const std::string& positions) {
+    if (positions.size() < 2)
+        return 0;
 
-        EXPECT_EQ(resulting_tokens, expected_tokens);
+    int posLen = positions.size();
+
+    // Skip all first L and last R
+    int lpos = 0;
+    while (lpos < posLen && positions[lpos] == 'L')
+        lpos++;
+    int rpos = posLen - 1;
+    while (rpos != 0 && positions[rpos] == 'R')
+        rpos--;
+
+    if (lpos == rpos)
+        return 0;
+
+    int count = 0;
+    for (int i = lpos; i <= rpos; i++) {
+        count += (positions[i] != 'S');
     }
+
+    return count;
 }
 
-TEST_F(TokenizeText, SplitWithLibs) {
-    char delimiter = ',';
+class StringIntParams: public ::testing::TestWithParam<std::pair<std::string, int>> {};
 
-    for (auto& str_param : param_tokens) {
-        const std::string& str = str_param[0];
-        VS expected_tokens(str_param.begin() + 1, str_param.end());
-        std::vector<std::string> resulting_tokens = SplitSimpleWithDelimiter(str, delimiter);
-
-        EXPECT_EQ(resulting_tokens, expected_tokens);
-    }
+TEST_P(StringIntParams, CheckCollision) {
+    auto [positions, res] = GetParam();
+    EXPECT_EQ(res, CheckCollision(positions));
 }
 
+INSTANTIATE_TEST_SUITE_P(
+    TwoPointers,        // Test suite name prefix
+    StringIntParams,    // Fixture class
+    ::testing::Values(  // Values
+        std::make_pair("RL", 2),
+        std::make_pair("LLLL", 0),
+        std::make_pair("RLRSLL", 5),
+        std::make_pair("RRR", 0),
+        std::make_pair("LLSRR", 0),
+        std::make_pair("", 0)
+    )
+);
